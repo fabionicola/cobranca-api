42d79d2ecacbdfbaa1b593331c2af5da065ea0b2
Fabio Nicola <90237498+fabionicola@users.noreply.github.com>
2026-02-03 08:04:50 -0300

    chore: organiza estrutura do projeto

---
+++ src/test/java/br/com/fabionicola/cobranca_api/CobrancaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabionicola.cobranca_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobrancaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
