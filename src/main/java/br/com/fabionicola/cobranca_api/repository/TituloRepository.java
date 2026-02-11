d9cf08c4ae99834e7f9af631658e5f0b7014d951
Fabio Nicola <90237498+fabionicola@users.noreply.github.com>
2026-02-11 11:21:36 -0300

    feat: adicionar consulta de atrasados em TituloRepository

--- src/main/java/br/com/fabionicola/cobranca_api/repository/TituloRepository.java
+++ src/main/java/br/com/fabionicola/cobranca_api/repository/TituloRepository.java
@@ -1,9 +1,15 @@
 package br.com.fabionicola.cobranca_api.repository;
 
 
+import br.com.fabionicola.cobranca_api.model.StatusTitulo;
 import br.com.fabionicola.cobranca_api.model.Titulo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
+
 public interface TituloRepository extends JpaRepository<Titulo, Long> {
+    List<Titulo> findByStatusAndVencimentoBefore(StatusTitulo status, LocalDate data);
 }
 
